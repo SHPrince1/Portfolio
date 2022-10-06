49118738c819b87a86f74a3dd549bdb1da49688d
Sylvanus Prince <princehsylvanus@gmail.com>
2022-10-06 17:17:07 +0100

    first

---
+++ index.js
@@ -0,0 +1,11 @@
+const menu = document.querySelector(".menu");
+const showUl = document.querySelector(".custom-ul");
+
+// const hideContent = document.querySelector('.hide')
+
+menu.addEventListener('click',  ()=>{
+showUl.classList.toggle('show')
+
+});
+
+
