a1d55cb17a1c81cf445b12445e4df6640db63c10
Sylvanus Prince <princehsylvanus@gmail.com>
2022-10-09 15:47:06 +0100

    js

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const menu = document.querySelector(".menu");
 const showUl = document.querySelector(".custom-ul");
+const dis = document.querySelector(".none");
+
 
 // const hideContent = document.querySelector('.hide')
 
 menu.addEventListener('click',  ()=>{
-showUl.classList.toggle('show')
+showUl.classList.toggle('show');
+
+
 
 });
+
+
+
+document.querySelectorAll(".none").forEach(n => n.
+    addEventListener('click', () =>{
+        menu.classList.remove('show');
+        showUl.classList.remove('show')
+    })
+    )
+
 
 
